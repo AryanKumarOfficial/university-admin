0573b0a96139c2696ca9cb6bba8d1786a8daa7d0
Aryan Kumar <aryanak9163@gmail.com>
2025-03-08 23:24:37 +0530

    adds analytics card to the Dashboard

--- src/app/(university)/page.js
+++ src/app/(university)/page.js
@@ -1,18 +1,15 @@
 import React from 'react'
 import Breadcrumb from "@/components/ui/Breadcrumb";
+import DataCard from "@/components/sections/Dashboard/DataCard";
 
 export default function Page(props) {
 
     return (
         <div className={"page"}>
             <Breadcrumb/>
-            <div className="section-body mt-4 min-vh-100">
-                <div className="container-fluid d-flex flex-column justify-content-center align-items-center" style={{
-                    height: "100vh"
-                }}>
-                    <h1 className={"page-subtitle fs-1 fw-bolder"}>Dashboard</h1>
-
-                    <h3 className={"page-subtitle fs-3 fw-bolder"}>Welcome to the University Admin Dashboard</h3>
+            <div className="section-body mt-4">
+                <div className="container-fluid">
+                    <DataCard/>
                 </div>
             </div>
         </div>
---
+++ src/components/sections/Dashboard/DataCard.js
@@ -0,0 +1,76 @@
+const DataCard = () => {
+    return (
+        <div className="section-body mt-4">
+            <div className="container-fluid">
+                <div className="row clearfix row-deck">
+                    <div className="col-lg-3 col-md-6 col-sm-6">
+                        <div className="card overflow-hidden">
+                            <div className="card-body">
+                                <div className="mb-2 h6">New Students</div>
+                                <div><span className="h4 font700">758</span> <span
+                                    className="small text-danger"><i
+                                    className="fa fa-level-down"></i> 08%</span></div>
+                                <small className="text-muted">Analytics for last week</small>
+                            </div>
+                            <div className="progress" style={{height: "4px"}}>
+                                <div className="progress-bar bg-danger" role="progressbar"
+                                     style={{width: "35%"}}
+                                     aria-valuenow="35" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-3 col-md-6 col-sm-6">
+                        <div className="card overflow-hidden">
+                            <div className="card-body">
+                                <div className="mb-2 h6">Total Courses</div>
+                                <div><span className="h4 font700">125</span> <span
+                                    className="small text-success"><i
+                                    className="fa fa-level-up"></i> 24%</span></div>
+                                <small className="text-muted">Analytics for last week</small>
+                            </div>
+                            <div className="progress" style={{height: "4px"}}>
+                                <div className="progress-bar bg-success" role="progressbar"
+                                     style={{width: "50%"}}
+                                     aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-3 col-md-6 col-sm-6">
+                        <div className="card overflow-hidden">
+                            <div className="card-body">
+                                <div className="mb-2 h6">Total Teachers</div>
+                                <div><span className="h4 font700">89</span> <span
+                                    className="small text-success"><i
+                                    className="fa fa-level-up"></i> 15%</span></div>
+                                <small className="text-muted">Analytics for last week</small>
+                            </div>
+                            <div className="progress" style={{height: "4px"}}>
+                                <div className="progress-bar bg-success" role="progressbar"
+                                     style={{width: "85%"}}
+                                     aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-3 col-md-6 col-sm-6">
+                        <div className="card overflow-hidden">
+                            <div className="card-body">
+                                <div className="mb-2 h6">Fees Collection</div>
+                                <div><span className="h4 font700">$48,697</span> <span
+                                    className="small text-danger"><i
+                                    className="fa fa-level-up"></i> 18%</span></div>
+                                <small className="text-muted">Analytics for last week</small>
+                            </div>
+                            <div className="progress" style={{height: "4px"}}>
+                                <div className="progress-bar bg-danger" role="progressbar"
+                                     style={{width: "50%"}}
+                                     aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default DataCard;
