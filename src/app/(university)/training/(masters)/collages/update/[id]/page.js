66a33b870871f38bd3e9f2f34646e170fac09b2e
Aryan Kumar <aryanak9163@gmail.com>
2025-04-07 22:30:22 +0530

    modified update collages masters form

--- src/app/(university)/training/(masters)/collages/update/[id]/page.js
+++ src/app/(university)/training/(masters)/collages/update/[id]/page.js
@@ -3,12 +3,12 @@ import Breadcrumb from "@/components/ui/Breadcrumb";
 import React, {useEffect, useState} from "react";
 import {useForm} from "react-hook-form";
 import {zodResolver} from "@hookform/resolvers/zod";
-import {LocationSchema} from "@/schema/Location";
 import {db} from "@/lib/firebase/client";
 import {doc, getDoc, updateDoc} from "firebase/firestore";
 import {useRouter} from "next/navigation";
 import {toast} from "react-hot-toast";
-import BasicInfoSection from "@/components/sections/training/masters/location/BasicInformation";
+import BasicInfoSection from "@/components/sections/training/masters/college/BasicInformation";
+import {CollageSchema} from "@/schema/Collage";
 
 export default function UpdateCollage({params}) {
     const {id} = params;
@@ -17,14 +17,18 @@ export default function UpdateCollage({params}) {
         register,
         handleSubmit,
         reset,
+        control,
         formState: {errors},
     } = useForm({
-        resolver: zodResolver(LocationSchema),
-        defaultValues: {name: ""},
+        resolver: zodResolver(CollageSchema),
+        defaultValues: {
+            name: "",
+            location: "",
+        },
         mode: "onChange",
     });
     const fetchLocation = async () => {
-        const docRef = await doc(db, "college-master", id);
+        const docRef = await doc(db, "collage-master", id);
         const docSnap = await getDoc(docRef);
         if (docSnap.exists()) {
             const data = docSnap.data();
@@ -44,14 +48,14 @@ export default function UpdateCollage({params}) {
             ...data,
         };
 
-        const docRef = doc(db, "college-master", id);
+        const docRef = doc(db, "collage-master", id);
         const promise = updateDoc(docRef, locationData);
 
         toast
             .promise(promise, {
-                loading: "updating Collage...",
-                success: "Collage updated successfully",
-                error: "Error updating Collage",
+                loading: "updating College...",
+                success: "College updated successfully",
+                error: "Error updating College",
             })
             .then(() => {
                 reset();
@@ -84,6 +88,7 @@ export default function UpdateCollage({params}) {
                                     register={register}
                                     errors={errors}
                                     title="Courses"
+                                    control={control}
                                 />
                                 <div className="d-flex justify-content-end gap-2 mb-5">
                                     <button
